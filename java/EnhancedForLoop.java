e36b9b341ae7f97e968cc533efe5631f4175c8d7
alsomilo <yuan.wdc@gmail.com>
2018-02-16 01:22:38 -0800

    02/16 fix indentation

--- java/EnhancedForLoop.java
+++ java/EnhancedForLoop.java
@@ -1,7 +1,6 @@
 /*The enhanced for loop declares a variable of a type compatible with the elements of the array being accessed. The variable will be available within the for block, and its value will be the same as the current array element.
 So, on each iteration of the loop, the variable t will be equal to the corresponding element in the array.*/
 
-
 public class EnhancedForLoop {
     public static void main(String[] args) {
 
@@ -17,8 +16,8 @@ public class EnhancedForLoop {
         
         for (int[] t: primes2) 
         {
-	    for (int q: t)     
-            System.out.println(q); 
+        	for (int q: t)     
+        		System.out.println(q); 
         }
     }
 }
